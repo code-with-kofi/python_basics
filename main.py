7b566bd4602b2552f6bcef9b481dc9c835fc27b8
EatSalone <eatsalone2022@gmail.com>
2023-02-09 01:24:19 +0000

    this is a asic calculator

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
-print("Hello, World!")
+opr1=0
+opr2=0
+operator=0
+print("\n\n\n\n")
+opr1 = int (input("\nenter first value : "))
+ 
+opr2 = int (input("\nenter second value : "))
+
+operator = int (input("\nenter \n1. to add \n2. to minus \n3 to multiply \n4 to divide \n  >>> "))
+
+print("result : ")
+if(operator == 1 ):
+    print(opr1+opr2)
+
+elif(operator == 2 ):
+    print(opr1-opr2)
+
+elif(operator == 3 ):
+    print(opr1*opr2)
+
+elif(operator == 1 ):
+    print(opr1/opr2)
+else:
+    print("please enter correct value")
+
+
+
+
+
+  
