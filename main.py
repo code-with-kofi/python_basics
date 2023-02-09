2eeb205cf6a437e370d5c8ad14dd7d6add56231b
abdulai90500 <abdulaiskamara4@gmail.com>
2023-02-09 10:19:50 -0800

    assignment slove

--- main.py
+++ main.py
@@ -4,12 +4,15 @@ operator=0
 again = "y"
 while again == "y":
     try:
+        
         print("\n\n\n\n")
         opr1 = int (input("\nenter first value : "))
         
         opr2 = int (input("\nenter second value : "))
 
-        operator = int (input("\nenter \n1. to add \n2. to minus \n3 to multiply \n4 to divide \n  >>> "))
+        operator = int (input("\nenter \n1. to add \n2. to minus \n3 to multiply \n4 to divide \n5 modulo \n6 exponitial \n  >>> "))
+        
+        
 
         print("result : ")
         if(operator == 1 ):
@@ -23,11 +26,18 @@ while again == "y":
 
         elif(operator == 4 ):
             print(opr1/opr2)
+        elif(operator ==5):
+            print(opr1%opr2)
+        elif(operator==6):
+            print(opr1**opr2)
         else:
             print("please enter correct value")
 
-        print("do you want to continue")
+        print("do you want to continue ? or press -1 to exit")
         again =input()
+        if again == -1:
+            print("Quiting.....")
+            break
     except:
         input(" you have enter a wrong value press any key to break")
         break
