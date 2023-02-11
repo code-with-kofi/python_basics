99954c5e8ea90c51555949fe2e0fe220ebd45709
EatSalone <eatsalone2022@gmail.com>
2023-02-11 02:27:19 +0000

    update

--- main.py
+++ main.py
@@ -1,51 +1,53 @@
-opr1=0
-opr2=0
-operator=0
-again = "y"
-while again == "y":
-    try:
-        
-        print("\n\n\n\n")
-        opr1 = int (input("\nenter first value : "))
-        
-        opr2 = int (input("\nenter second value : "))
+def main(): 
+    opr1=0
+    opr2=0
+    operator=0
+    again = "y"
+    while again == "y":
+        try: 
 
-        operator = int (input("\nenter \n1. to add \n2. to minus \n3 to multiply \n4 to divide \n5 modulo \n6 exponitial \n  >>> "))
-        
-        
+            print("\n\n\n\n")   
+            opr1 = int (input("\nenter first value : "))  
+            validator(opr1)  
+            opr2 = int (input("\nenter second value : "))
+            validator(opr2)
+            operator = int (input("\nenter \n1. to add \n2. to minus \n3 to multiply \n4 to divide \n5 modulo \n6 exponitial \n  >>> "))
+            validator(opr2)
+            
 
-        print("result : ")
-        if(operator == 1 ):
-            print(opr1+opr2)
+            print("result : ")
+            if(operator == 1 ):
+                print(opr1+opr2)
 
-        elif(operator == 2 ):
-            print(opr1-opr2)
+            elif(operator == 2 ):
+                print(opr1-opr2)
 
-        elif(operator == 3 ):
-            print(opr1*opr2)
+            elif(operator == 3 ):
+                print(opr1*opr2)
 
-        elif(operator == 4 ):
-            print(opr1/opr2)
-        elif(operator ==5):
-            print(opr1%opr2)
-        elif(operator==6):
-            print(opr1**opr2)
-        else:
-            print("please enter correct value")
+            elif(operator == 4 ):
+                print(opr1/opr2)
+                
+            elif(operator ==5):
+                print(opr1%opr2)
 
-        print("do you want to continue ? or press -1 to exit")
-        again =input()
-        if again == -1:
-            print("Quiting.....")
-            break
-    except:
-        input(" you have enter a wrong value press any key to break")
-        break
+            elif(operator==6):
+                print(opr1**opr2)
+            else:
+                print("please enter correct value")
 
-
-
-
+        except Exception as e :
+            if(len(e.args)>0):
+               input(e.args[0])
+               break
+            else:
+              validator(input(" you have enter a wrong value press any key to restart"))
+              continue
 
 
+def validator(value):
+    if(value == -1):
+         raise ValueError("You have chosen to exit the app")
 
-  
+
+main()
